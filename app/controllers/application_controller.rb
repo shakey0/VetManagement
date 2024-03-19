0c774ea88dd38c8cb7138a70f17513a1365d0b71
Andrew <shakeyshakespeare0@gmail.com>
2024-03-19 22:49:25 +0000

    Admin added to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        # Here you specify any additional parameters you want to permit for Devise (e.g., for sign up and account updating)
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :position])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :position])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone, :position, :password, :password_confirmation, :is_admin])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone, :position, :password, :password_confirmation, :is_admin])
     end
 end
